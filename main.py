ff018cdd064a4aec1c594eb35dd43929eb721b56
Fattahov_d <damir802@yandex.ru>
2024-03-27 20:26:05 +0500

    oop - nasledovanie

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
-class Stack:
-    def __init__(self):
-        self.__stackList = []
+# создать класс животные у которого есть свойство ходить
+#
+# создать подкласс кот у которого есть наследованное
+# свойство ходить и свой  свойство говорить Мяу
 
-    def push(self, val):
-        self.__stackList.append(val)
-
-    def pop(self):
-        val = self.__stackList[-1]
-        del self.__stackList[-1]
-        return val
-
-
-class AddingStack(Stack):
-    def __init__(self):
-        Stack.__init__(self)
-        self.__sum = 0
-
-    def getSum(self):
-        return self.__sum
-
-    def push(self, val):
-        self.__sum += val
-        Stack.push(self, val)
-
-    def pop(self):
-        val = Stack.pop(self)
-        self.__sum -= val
-        return val
-
-stackObject = AddingStack()
-for i in range(5):
-    stackObject.push(i)
-    print(stackObject.getSum())
-
-
-for i in range(5):
-    print(stackObject.pop())
+# class Stack:
+#     def __init__(self):
+#         self.__stackList = []
+#
+#     def push(self, val):
+#         self.__stackList.append(val)
+#
+#     def pop(self):
+#         val = self.__stackList[-1]
+#         del self.__stackList[-1]
+#         return val
+#
+# class AddingStack(Stack):
+#     def __init__(self):
+#         Stack.__init__(self)
+#         self.__sum = 0
+#
+#     def getSum(self):
+#         return self.__sum
+#
+#     def push(self, val):
+#         self.__sum += val
+#         Stack.push(self, val)
+#
+#     def pop(self):
+#         val = Stack.pop(self)
+#         self.__sum -= val
+#         return val
+#
+# stackObject = AddingStack()
+# for i in range(5):
+#     stackObject.push(i)
+#     print(stackObject.getSum())
+#
+#
+# for i in range(5):
+#     print(stackObject.pop())
